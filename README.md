6a45242218af8f58cdf2de3b011e6384d8ab4124
atharvnegi <atharvnegi2951@gmail.com>
2022-03-15 01:35:47 +0000

    Add extras for m10ltedd-user 10 QP1A.190711.020 M105FDDS4CUD1 release-keys
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## m10ltedd-user 10 QP1A.190711.020 M105FDDS4CUD1 release-keys
+- Manufacturer: samsung
+- Platform: universal7870
+- Codename: m10lte
+- Brand: samsung
+- Flavor: m10ltedd-user
+- Release Version: 10
+- Id: QP1A.190711.020
+- Incremental: M105FDDS4CUD1
+- Tags: release-keys
+- CPU Abilist: armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: 320
+- Fingerprint: samsung/m10ltedd/m10lte:10/QP1A.190711.020/M105FDDS4CUD1:user/release-keys
+- OTA version: 
+- Branch: m10ltedd-user-10-QP1A.190711.020-M105FDDS4CUD1-release-keys
+- Repo: samsung_m10lte_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/priv-app/Velvet/Velvet.apk
+rm -f system/system/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null >> system/system/priv-app/SecSettings/SecSettings.apk
+rm -f system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null
+cat system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
